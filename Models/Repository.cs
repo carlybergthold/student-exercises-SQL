5ff756a8992b5309b7f8b0f6b5e0c4d8103e46b0
Carly Bergthold <46655107+carlybergthold@users.noreply.github.com>
2019-07-30 09:34:04 -0700

    database hooked up

---
+++ Models/Repository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using StudentExercises5.Models;
+
+
+namespace StudentExercises5.Data
+{
+    public class Repository
+    {
+        public SqlConnection Connection
+        {
+            get
+            {
+                string _connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=StudentExercises;Integrated Security=True";
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+        public List<Exercise> GetExercises()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, ExerciseName, Language FROM Exercises";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Exercise> exercises = new List<Exercise>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int languageColumnPosition = reader.GetOrdinal("Language");
+                        string languageValue = reader.GetString(languageColumnPosition);
+
+                        int nameColumnPosition = reader.GetOrdinal("ExerciseName");
+                        string nameValue = reader.GetString(nameColumnPosition);
+
+                        Exercise exercise = new Exercise
+                        {
+                            Id = idValue,
+                            ExerciseName = nameValue,
+                            Language = languageValue
+                        };
+
+                        exercises.Add(exercise);
+                    }
+
+                    reader.Close();
+
+                    return exercises;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StudentExercises5.Data;
+using System.Collections.Generic;
+using System.Linq;
+using StudentExercises5.Models;
+
+namespace StudentExercises5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Repository repository = new Repository();
+            List<Exercise> exercises = repository.GetExercises();
+            exercises.ForEach(ex => Console.WriteLine(ex.ExerciseName));
+        }
+    }
+}
